d1dcd95e3f39a58eb975c8a3e6de2f5ac064f8bb
Amy Winkler <amy@Amys-Air.hsd1.ca.comcast.net>
2025-01-17 17:02:43 -0800

    gis map html

---
+++ styles/AssessorParcels_1_style.js
@@ -0,0 +1,30 @@
+var size = 0;
+var placement = 'point';
+
+var style_AssessorParcels_1 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; var value = feature.get("");
+    var labelFont = "13.0px \'Open Sans\', sans-serif";
+    var labelFill = "#323232";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if (feature.get("SITEADD") !== null) {
+        labelText = String(feature.get("SITEADD"));
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
