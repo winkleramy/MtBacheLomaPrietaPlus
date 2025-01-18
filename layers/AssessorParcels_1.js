d918592a48d48b14bf9ba77645420db00ff10acf
Amy Winkler <amy@Amys-Air.hsd1.ca.comcast.net>
2025-01-17 17:19:24 -0800

    without Assessors parcels

--- layers/AssessorParcels_1.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:807578a12fe9d7ff14b9939d7e666ca12313dea93d3dbc34e0c0354f887fdc54
-size 189608845
--- styles/WaterSources_2_style.js
+++ styles/WaterSources_2_style.js
@@ -1,26 +1,27 @@
 var size = 0;
 var placement = 'point';
 
-var style_AssessorParcels_1 = function(feature, resolution){
+var style_WaterSources_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
     };
     
     var labelText = ""; var value = feature.get("");
-    var labelFont = "13.0px \'Open Sans\', sans-serif";
-    var labelFill = "#323232";
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
     var textAlign = "left";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
-    if (feature.get("SITEADD") !== null) {
-        labelText = String(feature.get("SITEADD"));
+    if ("" !== null) {
+        labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
+        image: new ol.style.Circle({radius: 8.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.0}), fill: new ol.style.Fill({color: 'rgba(72,123,182,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
