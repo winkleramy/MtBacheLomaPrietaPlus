c6c2d2ef53e6f441aa07b79c3537bdadf8c6c666
Amy Winkler <amy@Mac.hsd1.ca.comcast.net>
2025-08-06 15:24:08 -0700

    Created Downloadable Map

--- styles/AssessorParcels_1_style.js
+++ styles/AssessorParcels_1_style.js
@@ -13,12 +13,12 @@ var style_AssessorParcels_1 = function(feature, resolution){
     var labelFill = "#323232";
     var bufferColor = "";
     var bufferWidth = 0;
-    var textAlign = "left";
+    var textAlign = "center";
     var offsetX = 0;
     var offsetY = 0;
     var placement = 'point';
-    if (feature.get("SITEADD") !== null) {
-        labelText = String(feature.get("SITEADD"));
+    if (feature.get("SITEADDNO") !== null) {
+        labelText = String(feature.get("SITEADDNO"));
     }
     var style = [ new ol.style.Style({
         stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.988}),
